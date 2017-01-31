dfc2d0400c26de9693ea2fd90a9d212c8aac1fb1
Adron Hall <adronhall@gmail.com>
2017-01-31 09:13:44 -0800

    Just adding a commit script, which I'll eventually expand to include a local build element.

--- gobuild.sh
+++ gobuild.sh
@@ -1,8 +1,11 @@
+#!/usr/bin/env bash
 
-
+# Parameters for commit message and branch to commit to.
 commitmessage=$1
 branch=$2
 
+# Commit and push to X branch.
 git add -A
 git commit -m '$commitmessage'
 git push origin $branch
+
